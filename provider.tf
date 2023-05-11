6f4bee46718708be6e43d1ebf100ea816ce8b5f4
deva0209 <120808857+deva0209@users.noreply.github.com>
2023-05-11 13:39:15 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIA2YZKYXEGYAMIMAQA"
-  secret_key = "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"
+  access_key = "AKIAUEQBJJJT7GCIZ7PN"
+  secret_key = "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"
 }
