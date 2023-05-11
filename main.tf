5aa8e5a0485986fae7096c3e868db84d937dbd73
deva0209 <120808857+deva0209@users.noreply.github.com>
2023-05-11 13:21:59 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "test-server" {
+  ami           = "ami-03a933af70fa97ad2" 
+  instance_type = "t2.medium"
+  key_name = "gnan"
+  vpc_security_group_ids= ["sg-09e2c8be5cc7b0db2"]
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    private_key = file("./gnan.pem")
+    host     = self.public_ip
+  }
+  provisioner "remote-exec" {
+    inline = [ "echo 'wait to start instance' "]
+  }
+  tags = {
+    Name = "test-server"
+  }
+  provisioner "local-exec" {
+        command = " echo ${aws_instance.test-server.public_ip} > inventory "
+		}
+  provisioner "local-exec" {
+  command = "ansible-playbook /var/lib/jenkins/workspace/finance/serverfiles/bankingplaybook.yml"
+  } 
+}
