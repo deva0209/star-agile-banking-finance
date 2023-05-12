1c92545cd565657d757f9c828d3cb0c808702523
deva0209 <120808857+deva0209@users.noreply.github.com>
2023-05-12 15:32:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "AKIAUEQBJJJTRK4MZGO7"
+  secret_key = "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"
+}
 resource "aws_instance" "test-server" {
   ami           = "ami-0d683450f458cdbaa" 
   instance_type = "t2.micro"
